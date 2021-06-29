407b3e39f05b56f7dd24de30b39e33c4cf30c113
Osmania <oslab@oslab.ewha.ac.kr>
2021-06-29 12:33:09 +0900

    support multiple instances via mlstorctl.sh

--- script/mlstorctl.sh
+++ script/mlstorctl.sh
@@ -7,9 +7,29 @@ Usage: mlstorctl -c -b <backing device> -s <nn size> <mlstor name>
 EOF
 }
 
+MGMT=/sys/kernel/stolearn-nn/mgmt
+
 function get_sectors() {
     size=`lsblk -bln -o SIZE $1 | head -n 1`
     expr $size / 512
+}
+
+function get_cur_nns() {
+    (cat $MGMT | grep -Eo '^stolearn-nn[[:digit:]]+' | grep -Eo [[:digit:]]+ | sort) 2> /dev/null
+}
+
+function guess_new_nn() {
+    for nn in `get_cur_nns`; do
+	echo $1 | grep -wq $nn
+	if [ $? -ne 0 ]; then
+	    echo $nn
+	    exit
+	fi
+    done
+}
+
+function get_attached_nn() {
+    (dmsetup table $1 | grep -Eo 'stolearn-nn[[:digit:]]+' | grep -Eo [[:digit:]]+) 2> /dev/null
 }
 
 function create_mlstor() {
@@ -17,15 +37,18 @@ function create_mlstor() {
 	echo "stolearn-nn kernel module not installed"
 	exit 3
     fi
+    nns=`get_cur_nns`
     echo "stolearn-nn attach $2" > /sys/kernel/stolearn-nn/mgmt
+    nn=`guess_new_nn "$nns"`
     cachesize=`expr $2 \* 8`
     sectors=`get_sectors $1`
-    echo "0 $sectors stolearn-cache $1 /dev/stolearn-nn0 $cachesize" | dmsetup create $3
+    echo "0 $sectors stolearn-cache $1 /dev/stolearn-nn$nn $cachesize" | dmsetup create $3
 }
 
 function delete_mlstor() {
+    nn=`get_attached_nn $1`
     dmsetup remove $1
-    echo "stolearn-nn detach 0" > /sys/kernel/stolearn-nn/mgmt
+    echo "stolearn-nn detach $nn" > /sys/kernel/stolearn-nn/mgmt
 }
 
 if [[ $EUID -ne 0 ]]; then
@@ -73,6 +96,14 @@ fi
 name=$1
 
 if [[ -n $create ]]; then
+    if [[ -z $backdev ]]; then
+	echo "backing device is required"
+	exit 3
+    fi
+    if [[ -z $size ]]; then
+	echo "size is required"
+	exit 3
+    fi
     create_mlstor $backdev $size $name
 elif [[ -n $delete ]]; then
     delete_mlstor $name
